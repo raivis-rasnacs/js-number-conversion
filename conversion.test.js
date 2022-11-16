e6316cdeaa045d13d6be7af3edaa7f34578b0180
Raivis <71266644+raivis-rasnacs@users.noreply.github.com>
2022-11-16 22:11:30 +0200

    Create conversion.test.js

---
+++ conversion.test.js
@@ -0,0 +1,14 @@
+const conversion = require('./conversion');
+
+function testing_3() {
+  expect(conversion(3).toBe("11"));
+}
+
+function testing_38() {
+  expect(conversion(38).toBe("100110"));
+  
+function_testing_1111() {
+  expect(conversion("1111").toBe(15));
+
+test("checks conversion from 3 to binary", testing_3);
+
