b373abe853b8d138703f82467afc9b9be9088a89
Raivis <71266644+raivis-rasnacs@users.noreply.github.com>
2022-11-16 23:38:50 +0200

    Update conversion1.test.js

--- conversion1.test.js
+++ conversion1.test.js
@@ -1,7 +1,7 @@
 const conversion = require('./conversion');
 
 function testing_3() {
-  expect(conversion(3).toBe("11"));
+  expect(conversion(3)).toBe("11");
 }
 
 test("checks conversion from 3 to binary", testing_3);
