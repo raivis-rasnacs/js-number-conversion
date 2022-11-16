8db2c93cf448712c2e60753ef49ecafbbfdf1a09
Raivis <71266644+raivis-rasnacs@users.noreply.github.com>
2022-11-16 22:24:13 +0200

    Update conversion1.test.js

--- conversion1.test.js
+++ conversion1.test.js
@@ -4,15 +4,5 @@ function testing_3() {
   expect(conversion(3).toBe("11"));
 }
 
-function testing_38() {
-  expect(conversion(38).toBe("100110"));
-}
-  
-function testing_1111() {
-  expect(conversion("1111").toBe(15));
-}
-
 test("checks conversion from 3 to binary", testing_3);
-test("checks conversion from 38 to binary", testing_38);
-test("checks conversion from '1111' to decimal", testing_1111);
 
